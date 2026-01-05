6e47ae2153a230691d165937a5b70395c7090d22
Afzaal Suleman <afzaaladi48@gmail.com>
2026-01-05 23:29:56 +0500

    error handling

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,16 @@ import Products from '@/components/Products';
 import { getCategories, getProducts } from '@/services/productService';
 export default async function Home() {
 
-  const products = await getProducts()
-  const categories = await getCategories();
-  return (
-    <div>
-      <Products products={products} categories={categories} />
-    </div>
-  );
+  try {
+    const products = await getProducts()
+    const categories = await getCategories();
+    return <Products products={products} categories={categories} />;
+  } catch (error) {
+    console.error("Failed to fetch products:", error);
+    return (
+      <div className="text-center text-red-500 mt-10">
+        Failed to load products. Please try again later.
+      </div>
+    );
+  }
 }
