c9b047d5242956b1d1448606be65f46c43609abf
Afzaal Suleman <afzaaladi48@gmail.com>
2026-01-06 00:51:09 +0500

    new changes

--- app/productDetails/[id]/page.tsx
+++ app/productDetails/[id]/page.tsx
@@ -1,22 +1,67 @@
+"use client";
+import { useEffect, useState } from "react";
+import { useParams } from "next/navigation";
 import { getProductById } from "@/services/productService";
-type Props = {
-  params: {
-    id: string;
-  };
+
+import Loader from "@/components/Loader";
+import { useCartStore } from "@/store/useCartStore";
+
+type ProductType = {
+  id: number;
+  title: string;
+  price: number;
+  description: string;
+  category: string;
+  image: string;
+  rating: { rate: number; count: number };
 };
+export default function ProductDetail() {
+  const params: any = useParams();
+  const id = params.id;
+  const [product, setProduct] = useState<ProductType | null>(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
+  const { addToCart } = useCartStore();
+  const addItemsToCart = (product: ProductType) => {
+    addToCart({
+      id: product.id,
+      title: product.title,
+      price: product.price,
+      image: product.image,
+    });
+  };
+  useEffect(() => {
+    async function fetchProduct() {
+      try {
+        setLoading(true);
+        const data = await getProductById(id);
+        setProduct(data);
+      } catch (err) {
+        setError("Failed to load product");
+      } finally {
+        setLoading(false);
+      }
+    }
 
-export default async function ProductDetail({ params }: Props) {
-  const { id } = await params;
-  console.log(id, "id");
+    fetchProduct();
+  }, [id]);
 
-  const product = await getProductById(id)
-  
-  
+  if (loading) {
+    return <Loader />;
+  }
+
+  if (error) {
+    return <p className="text-center mt-10 text-red-500">{error}</p>;
+  }
+
+  if (!product) {
+    return <p className="text-center mt-10 text-gray-500">Product not found</p>;
+  }
 
   return (
-   <div className="max-w-6xl mx-auto px-4 py-10">
+    <div className="max-w-6xl mx-auto px-4 py-10">
       <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
-        
+
         {/* Product Image */}
         <div className="flex justify-center bg-gray-100 rounded-xl p-6">
           <img
@@ -57,7 +102,9 @@ export default async function ProductDetail({ params }: Props) {
 
           {/* Actions */}
           <div className="flex gap-4 pt-4">
-            <button className="px-6 py-3 bg-black text-white rounded-lg hover:bg-gray-800 transition">
+            <button
+              onClick={() => addItemsToCart(product)}
+              className="px-6 py-3 bg-black text-white rounded-lg hover:bg-gray-800 transition">
               Add to Cart
             </button>
             <button className="px-6 py-3 border border-black rounded-lg hover:bg-black hover:text-white transition">
